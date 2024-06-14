9c6e77df6a7e67814643e2532b5e4c5be129a045
Mishragini <mishragini8@gmail.com>
2024-06-14 19:28:28 +0530

    landing page init

---
+++ stellar/src/components/Landing.jsx
@@ -0,0 +1,9 @@
+export default function LandingPage() {
+    return (
+        <div className="bg-[#111827] grid grid-cols-2 px-10 py-4">
+        <div className="">
+        <div className="text-white text-8xl font-semibold">Unlock Your Potential with Decentr</div>
+        </div>
+        </div>
+    )
+}
