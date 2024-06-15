1b8ae26a214a541f27dd36695eb7470a1dfa3bb2
Mishragini <mishragini8@gmail.com>
2024-06-15 09:39:48 +0530

    adds the basic pages with types to it

--- react-fe/src/App.tsx
+++ react-fe/src/App.tsx
@@ -1,14 +1,22 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import { Button } from './components/ui/button'
+import LandingPage from './components/Landing'
+import SignIn from './components/SignIn'
+import SignupComp from './components/Signup'
 
 function App() {
 
 
   return (
-    <>
-      <Button>Click me</Button>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage/>}/>
+      <Route path="/signin" element={<SignIn/>}/>
+      <Route path="/signup" element={<SignupComp/>}/>
+    </Routes>
+    </BrowserRouter>
+    
   )
 }
 
