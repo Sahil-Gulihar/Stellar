46c0948d0bf0eae92e3ce4558152157b74667519
kairveeehh <jampu741@gmail.com>
2024-06-15 17:36:15 +0530

    client dashboard

--- react-fe/src/App.tsx
+++ react-fe/src/App.tsx
@@ -1,9 +1,12 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import LandingPage from './components/Landing'
+ import LandingPage from './components/Landing'
 import SignIn from './components/SignIn'
 import SignupComp from './components/Signup'
+
+
+
 
 function App() {
 
@@ -11,7 +14,7 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<LandingPage/>}/>
+      <Route path="/" element={<LandingPage />}/>
       <Route path="/signin" element={<SignIn/>}/>
       <Route path="/signup" element={<SignupComp/>}/>
     </Routes>
